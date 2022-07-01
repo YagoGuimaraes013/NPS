0c4af3406193978231d8ded425f8a212072de4c8
YagoGuimaraes013 <108237133+YagoGuimaraes013@users.noreply.github.com>
2022-06-30 22:14:09 -0300

    Delete tutorial_javaee_basico-master directory

--- tutorial_javaee_basico-master/regescweb/build/web/WEB-INF/classes/br/edu/ifsp/regescweb/db/create_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Student(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) NOT NULL,
-    age INT,
-    
-    PRIMARY KEY(id)
-);
--- tutorial_javaee_basico-master/regescweb/build/web/students/error.jsp
+++
@@ -1,5 +0,0 @@
-<jsp:include page="_header.jsp" />
-
-<h1>Ocorreu um erro com o Banco de Dados!</h1>
-
-<jsp:include page="_footer.jsp" />
